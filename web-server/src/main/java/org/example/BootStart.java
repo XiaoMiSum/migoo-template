db1d94d974ca4e5b578a19fc8b8c8482a642c6db
xiaomi <mi_xiao@qq.com>
2024-04-21 20:44:24 +0800

    ✨: feat: 使用框架的基础服务

--- web-server/src/main/java/org/example/BootStart.java
+++ web-server/src/main/java/org/example/BootStart.java
@@ -3,7 +3,7 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"${migoo.info.base-package}"})
+@SpringBootApplication(scanBasePackages = {"${migoo.framework.package-name}", "${migoo.framework.biz-package-name}"})
 public class BootStart {
 
     public static void main(String[] args) {
